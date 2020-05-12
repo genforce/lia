d5f4f47ceb797b685d4223606fa94945e30a2a6b
zhujiapeng <jengzhu0@gmail.com>
2020-05-12 08:39:02 +0800

    Add reconstruct.py script.

---
+++ reconstruct.py
@@ -0,0 +1,75 @@
+"""
+Reconstruction given images using LIA, the reconstruction result will be saved.
+"""
+
+import tensorflow as tf
+import numpy as np
+import os
+from utils import imwrite, immerge
+from utils import preparing_data, load_pkl
+import sys
+import dnnlib
+import dnnlib.tflib as tflib
+from tqdm import tqdm
+import argparse
+
+
+
+def main():
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+    tf_config = {'rnd.np_random_seed': 1000}
+    tflib.init_tf(tf_config)
+    assert os.path.exists(args.restore_path)
+    E, _, _, Gs, _ = load_pkl(args.restore_path)
+    num_layers, latent_dim = Gs.components.synthesis.input_shape[1:3]
+
+    # building graph
+    real = tf.placeholder('float32', [None, 3, args.image_size, args.image_size], name='real_image')
+    encoder_w = E.get_output_for(real, phase=False)
+    encoder_w_tile = tf.tile(encoder_w[:, np.newaxis], [1, num_layers, 1])
+    reconstructor = Gs.components.synthesis.get_output_for(encoder_w_tile, randomize_noise=False)
+    sess = tf.get_default_session()
+
+    # preparing data
+    input_images, _ = preparing_data(im_path=args.data_dir_test, img_type=args.img_type)
+
+    save_dir = args.output_dir or './outputs/reconstruction'
+    os.makedirs(save_dir, exist_ok=True)
+
+    print('Reconstructing...')
+    for it, image_id in tqdm(enumerate(range(0, input_images.shape[0], args.batch_size))):
+        batch_images = input_images[image_id:image_id+args.batch_size]
+        rec = sess.run(reconstructor, feed_dict={real: batch_images})
+        orin_recon = np.concatenate([batch_images, rec], axis=0)
+        orin_recon = orin_recon.transpose(0, 2, 3, 1)
+        imwrite(immerge(orin_recon, 2, batch_images.shape[0]), '%s/reconstruction_%06d.png' % (save_dir, it))
+
+
+if __name__ == "__main__":
+
+    import signal
+    signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--restore_path', type=str, default='',
+                        help='The pre-trained encoder pkl file path')
+    parser.add_argument("--data_dir_test", type=str, default='',
+                        help="Location of the test data")
+    parser.add_argument("--img_type", type=str, default='.png',
+                        help="test images type, such as .jpg., .png")
+    parser.add_argument("--image_size", type=int,
+                        default=128, help="the training image size")
+    parser.add_argument("--batch_size", type=int,
+                        default=8, help="size of the input batch")
+    parser.add_argument('--output_dir', type=str, default='',
+                        help='Directory to save the results. If not specified, '
+                             '`./outputs/reconstruction` will be used by default.')
+    parser.add_argument('--gpu_id', type=str, default='0',
+                        help='Which GPU(s) to use. (default: `0`)')
+
+    args = parser.parse_args()
+    main()
+
+
